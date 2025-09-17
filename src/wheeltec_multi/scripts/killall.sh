ac6d13cb04d39a9cbf9351c830b13de3c4d798b4
creat <3102871557@qq.com>
2025-09-17 11:00:50 +0800

    save7

--- src/wheeltec_multi/scripts/killall.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-### BEGIN INIT
-gnome-terminal -- bash -c "killall -wg ros2"
-sleep 2
-
-wait
-exit 0
--- src/wheeltec_multi/scripts/leadercar.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-### BEGIN INIT
-gnome-terminal -- bash -c "source /opt/ros/humble/setup.bash;source /home/wheeltec/wheeltec_ros2/install/setup.bash;ros2 launch wheeltec_multi navigation.launch.py"
-sleep 10
-
-wait
-exit 0
